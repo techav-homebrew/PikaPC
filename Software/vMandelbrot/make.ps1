185239c8c9546c6f86b536fa9b9509b64ffc0613
techav <76832805+techav-homebrew@users.noreply.github.com>
2026-04-12 03:49:44 -0500

    add minimal linker script

--- Software/vMandelbrot/make.ps1
+++ Software/vMandelbrot/make.ps1
@@ -17,7 +17,8 @@ $files = "vMandelbrot.c"
 
 $gccop = "-std=gnu99 -mcpu=403 -nostdlib -nodefaultlibs -Wall"
 $gccop+= " -save-temps -O1 -Xlinker --entry=main"
-$gccop+= " -Xlinker -Ttext -Xlinker 0x7fe60000"
+# $gccop+= " -Xlinker -Ttext=0x7fe60000"
+$gccop+= " -Xlinker -T -Xlinker linker.ld"
 $gccop+= " -Xlinker -Map -Xlinker $src.map"
 $gccop+= " -Xassembler -a=`"$src.lst`""
 
@@ -25,9 +26,9 @@ $gasop = "-m403 -v --warn -a=`"$src.lst`""
 
 $gldop = "--entry=main -Ttext 0x7fe60000 -Map $src.map"
 
-$objop = "--srec-forceS3 --change-addresses 0x7fe60000"
+$objop = "--srec-forceS3" # --change-addresses 0x7fe60000"
 
 iex "$gcc $($gccop) $files"
 # iex "$gas $($gasop) -o $src.o $src.s"
 # iex "$gld $($gldop) $src.o"
-iex "$objcopy $objop $src.o -O srec $src.srec"
+iex "$objcopy $objop a.out -O srec $src.srec"
